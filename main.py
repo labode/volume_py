08cc37556d1fe46a38f7b84121119c0503b0d51b
Jonas Labode <labode.jonas@mh-hannover.de>
2021-06-10 13:27:41 +0200

    nrrd reading changed to separate header and data body, TODOs updated

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import nrrd
 import sys
 
 
+# TODO: supply voxel size and calculate volume here?
 def analyze(file):
-    # TODO: Read .nrrd
-    data = nrrd.read(file)
+    # Read .nrrd
+    data, header = nrrd.read(file)
     entries = np.unique(data)
     count = []
 
-    # TODO: For each label => Count number of pixels
+    # For each label => Count number of pixels
     for entry in entries:
         # We do not need the zeroes (= background label)
         if entry == 0:
@@ -23,10 +24,9 @@ def analyze(file):
     return count
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    # TODO: Get volume to analyze
+    # Get volume to analyze
     mha_file = sys.argv[1]
 
     error = False
@@ -34,10 +34,10 @@ if __name__ == '__main__':
         error = True
 
     if not error:
-        # TODO: Convert volume to .nrrd
+        # Convert volume to .nrrd
         nrrd_file = converter.convert(mha_file, 'volume')
         analysis = analyze(nrrd_file)
-        # TODO: Write .csv with the results
+        # Write .csv with the results
         csv_writer.write(analysis, 'sizes')
 
     else:
