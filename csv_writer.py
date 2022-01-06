015735ffe21ed9756051ea38edd22df950fef6d5
Jonas Labode <labode.jonas@mh-hannover.de>
2022-01-06 10:43:37 +0100

    Trailing separator in .csv output removed

--- csv_writer.py
+++ csv_writer.py
@@ -4,9 +4,9 @@ def write(data, target_file):
     output_file = open(filename, "w")
 
     # Write header
-    output_file.write("Id; Volume;\n")
+    output_file.write("Id; Volume\n")
 
     for i in data:
-        output_file.write(str(i[0]) + '; ' + str(i[1]) + ';\n')
+        output_file.write(str(i[0]) + '; ' + str(i[1]) + '\n')
 
     output_file.close()
